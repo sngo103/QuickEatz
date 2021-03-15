e9b0a5fea65de02bf0e562bd47adf61e61bf8366
Samantha Ngo <samantha.ngo04@myhunter.cuny.edu>
2021-03-14 21:14:48 -0400

    Added page files and tutorial pages

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>QuickEatz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          QuickEatz
         </h1>
 
         <p className="description">
