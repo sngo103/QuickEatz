78295a838465318c232b5562621960d9dbb5f617
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-03-22 14:12:22 -0400

    Added base files for login page and create account page

---
+++ pages/createAccount.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Create Account</title>
+      </Head>
+      <h1>Create Account Page</h1>
+    </Layout>
+  )
+}
---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login Page</h1>
+    </Layout>
+  )
+}
