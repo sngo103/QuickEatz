1bdd6c11d1502a7b72e4a5792019d06b62f09064
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-03-22 15:28:53 -0400

    Added page templates

---
+++ pages/editProfile.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Edit Vendor and Customer Profile</title>
+      </Head>
+      <h1>Edit Profiles Page</h1>
+    </Layout>
+  )
+}
