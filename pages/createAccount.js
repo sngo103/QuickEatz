54b64b30bda0e873344d78bb5135fba2703d3efa
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-27 15:27:04 -0400

    intialized the basics of the createAccount page and also created an Account css file in styles.

--- pages/createAccount.js
+++ pages/createAccount.js
@@ -1,14 +1,30 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
+import Account from '../styles/Account.module.css'
 
 export default function FirstPost() {
   return (
     <Layout>
+
       <Head>
         <title>Create Account</title>
       </Head>
+
       <h1>Create Account Page</h1>
+      <h1>Please Select a Vendor or Cusomer Account </h1>
+      <select onChange={this.handleColorChange}>
+        <option value="Customer">Customer</option>
+        <option value="Vendor">Vendor</option>
+      </select>
+
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username: <input type="text" name="username" />
+          Password: <input type="test" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </Layout>
   )
 }
