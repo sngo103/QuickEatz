056e6eff0177892d5c9953b042e5d1682203c095
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-27 18:02:08 -0400

    added a div around everything being returned to render to fix an error when adding the jsx style component

--- pages/createAccount.js
+++ pages/createAccount.js
@@ -5,62 +5,65 @@ import Account from '../styles/Account.module.css'
 
 export default function FirstPost() {
   return (
-    <Layout>
+    <div>
+      <Layout>
 
-      <Head>
-        <title>Create Account</title>
-      </Head>
+        <Head>
+          <title>Create Account</title>
+        </Head>
 
-      <h1>Create Account</h1>
-      <p></p>
-      <h1>Please Select a Vendor or Cusomer Account </h1>
-      <select onChange={this.handleColorChange}>
-        <option value="Customer">Customer</option>
-        <option value="Vendor">Vendor</option>
-      </select>
+        <h1>Create Account</h1>
+        <p></p>
+        <h1>Please Select a Vendor or Cusomer Account </h1>
+        <select onChange={this.handleColorChange}>
+          <option value="Customer">Customer</option>
+          <option value="Vendor">Vendor</option>
+        </select>
 
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Username: <input type="text" name="username" />
-          Password: <input type="test" name="password" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </Layout>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username: <input type="text" name="username" />
+            Password: <input type="test" name="password" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </Layout>
 
-    <jsx>{`
-      .usernameLabel {
-      border: #FFE66D 30px 30px 30px 30px;
-      outline: #000000 30px 30px 30px 30px;
-      }
-
-      .passwordLabel {
-      border: #51B1E7 30px 30px 30px 30px;
-      outline: #000000 30px 30px 30px 30px;
-      }
-
-      .topPage {
-      background-color: #FF6B6B;
-      }
-
-      .midPage {
-      background-color: #FFE66D;
-      }
-
-      .bottomPage {
-      background-color: #4ECDC4;
-      }
-
-      .enterUsername {
-      border: #FFE66D 30px 30px 30px 30px;
-      outline: #000000 30px 30px 30px 30px;
-      }
-
-      .enterPassword {
-      border: #51B1E7 30px 30px 30px 30px;
-      outline: #000000 30px 30px 30px 30px;
-      }
+      <style jsx>{`
       
-    `}</jsx>
+        .usernameLabel {
+        border: #FFE66D 30px 30px 30px 30px;
+        outline: #000000 30px 30px 30px 30px;
+        }
+
+        .passwordLabel {
+        border: #51B1E7 30px 30px 30px 30px;
+        outline: #000000 30px 30px 30px 30px;
+        }
+
+        .topPage {
+        background-color: #FF6B6B;
+        }
+
+        .midPage {
+        background-color: #FFE66D;
+        }
+
+        .bottomPage {
+        background-color: #4ECDC4;
+        }
+
+        .enterUsername {
+        border: #FFE66D 30px 30px 30px 30px;
+        outline: #000000 30px 30px 30px 30px;
+        }
+
+        .enterPassword {
+        border: #51B1E7 30px 30px 30px 30px;
+        outline: #000000 30px 30px 30px 30px;
+        }
+
+      `}</style>
+    </div>
   );
 }
