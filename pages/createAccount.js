8c0d849cdab265691db7383cbdbe9496596922b8
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-28 14:40:09 -0400

    added three sections with the colors of the webpage to start organizzing the webpage and also added some extra jsx features to the toppage section element

--- pages/createAccount.js
+++ pages/createAccount.js
@@ -8,30 +8,43 @@ export default function FirstPost() {
     <div>
       <Layout>
 
-        <Head>
-          <title>Create Account</title>
-        </Head>
+        <section class="topPage">
+          <Head>
+            <title>Create Account</title>
+          </Head>
+        </section>
 
-        <h1>Create Account</h1>
-        <p></p>
-        <h1>Please Select a Vendor or Cusomer Account </h1>
-        <select onChange={this.handleColorChange}>
-          <option value="Customer">Customer</option>
-          <option value="Vendor">Vendor</option>
-        </select>
+        <section class="midPage">
+          <h1>Create Account</h1>
+        </section>
 
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Username: <input type="text" name="username" />
-          </label>
-          <label>
-            Password: <input type="test" name="password" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+
+        <section class="bottomPage">
+
+          <h1>Please Select a Vendor or Cusomer Account </h1>
+          <select onChange={this.handleColorChange}>
+            <option value="Customer">Customer</option>
+            <option value="Vendor">Vendor</option>
+          </select>
+
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Username: <input type="text" name="username" />
+            </label>
+            <label>
+              Password: <input type="test" name="password" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+        </section>
       </Layout>
 
       <style jsx>{`
+
+        .title {
+          font: 30px "bungee-shade";
+        }
 
         .usernameLabel {
         border: #FFE66D 30px 30px 30px 30px;
@@ -45,6 +58,10 @@ export default function FirstPost() {
 
         .topPage {
         background-color: #FF6B6B;
+        width: 100%;
+        height: 25%;
+        display: flex;
+        align-items: center;
         }
 
         .midPage {
