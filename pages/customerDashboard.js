1405df9c63e7a9d182d431ebc30b6a03ab16da53
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-28 22:29:16 -0400

    uncommented remaining files.

--- pages/customerDashboard.js
+++ pages/customerDashboard.js
@@ -1,4 +1,3 @@
-/*
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
@@ -13,4 +12,3 @@ export default function FirstPost() {
     </Layout>
   )
 }
-*/
