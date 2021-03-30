95e356f6d69909d8e6fd2e08e83aa30b67c19318
TDLorenz <troydaniello.nyc@gmail.com>
2021-03-30 00:19:22 -0400

    added formatting for background color of Sign up page

--- pages/createAccount.js
+++ pages/createAccount.js
@@ -2,55 +2,55 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
 
+
 export default function FirstPost() {
   return (
-    <div class>
-      <Layout>
-        <Head>
+    <div>
 
-          <div class="navbar">
-            <a href="/">Home</a>
-            <a href="/login">Login</a>
-          </div>
-          <section class="topPage">
-            <title class="title">Create Account</title>
-          </section>
-        </Head>
+      <div className="navbar">
+        <a href="/">Home</a>
+        <a href="/login">Login</a>
+      </div>
 
-        <section class="midPage">
-          <h1>Create Account</h1>
-        </section>
+      <section className="topPage">
+        <Layout>
+          <Head>
+            <title className="title">Create Account</title>
+          </Head>
+          <h1 className="title">Create Account</h1>
+        </Layout>
+      </section>
 
+      <section className="midPage">
+        <h1>Please Select a Vendor or Customer Account </h1>
+      </section>
 
-        <section class="bottomPage">
+      <section className="bottomPage">
+        <select>
+          <option value="Customer">Customer</option>
+          <option value="Vendor">Vendor</option>
+        </select>
 
-          <h1>Please Select a Vendor or Customer Account </h1>
-          <select>
-            <option value="Customer">Customer</option>
-            <option value="Vendor">Vendor</option>
-          </select>
+        <form onSubmit>
+          <label className="usernameLabel">
+            Username: <input type="text" name="username" />
+          </label>
+          <div></div>
+          <label className="passwordLabel">
+            Password: <input type="test" name="password" />
+          </label>
+          <div></div>
+          <input type="submit" value="Submit" />
+        </form>
 
-          <form onSubmit>
-            <label class="usernameLabel">
-              Username: <input type="text" name="username" />
-            </label>
-            <div></div>
-            <label class="passwordLabel">
-              Password: <input type="test" name="password" />
-            </label>
-            <div></div>
-            <input type="submit" value="Submit" />
-          </form>
-
-        </section>
-      </Layout >
+      </section>
 
 
       <style jsx>{`
 
         .navbar {
           overflow: hidden;
-          background-color: #23272a;
+          background-color: #F7FFF7;
           padding: 10px;
           width: 100%;
           display: flex;
@@ -58,8 +58,27 @@ export default function FirstPost() {
           align-items: center;
         }
 
+        .navbar a {
+          float: left;
+          color: #000000;
+          text-align: center;
+          padding: 10px 10px 10px 10px;
+          text-decoration: none;
+          font-size: 30px;
+        }
+
+        .navbar a:hover {
+          background-color: #DDD;
+          color: black;
+        }
+        
+        .navbar a.active {
+          background-color: #7289DA;
+          color: white;
+        }
+
         .title {
-          font: 30px "Bungee Shade";
+          font: 60px "Bungee Shade";
         }
 
         .usernameLabel {
@@ -75,17 +94,26 @@ export default function FirstPost() {
         .topPage {
           background-color: #FF6B6B;
           width: 100%;
-          height: 25%;
+          height: 10%;
           display: flex;
           align-items: center;
+          text-align: center;
+          
         }
 
         .midPage {
           background-color: #FFE66D;
+          padding: 30px 30px 30px 30px;
+          align-items: center;
+          text-align: center;
         }
 
         .bottomPage {
           background-color: #4ECDC4;
+          align-items: center;
+          text-align: center;
+          height 75%;
+          padding 200px 200px 200px 200px;
         }
 
         .enterUsername {
@@ -99,9 +127,11 @@ export default function FirstPost() {
         }
 
       `}</style>
+
     </div >
   );
 }
 
 // <select onChange={this.handleChange} >
 // form onSubmit={this.handleSubmit}>
+
