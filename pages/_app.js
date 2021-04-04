78563ba8250e5945675ed713b89251664619e656
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-04 01:26:46 -0400

    Fixed up border

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import NavBar from '../components/NavBar.js'
+import "../styles/globals.css";
+import NavBar from "../components/NavBar.js";
 
 export default function App({ Component, pageProps }) {
   return (
-  <div>
-  <NavBar />
-  <Component {...pageProps} />
-  </div>
-  )
+    <div>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Pridi:wght@300&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap');
+      </style>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
