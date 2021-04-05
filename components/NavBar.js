4d9e27b8fa26268ac96ad09bdd03b9a9578601c0
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-05 00:35:37 -0400

    fixed links of trending, your profile and sign out in dropdown on navbar and also added bolierplate code to trending.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -18,22 +18,22 @@ export class NavBar extends React.Component {
                 <div className="flex-shrink-0 text-white font-bungee text-2xl"> <Link href="/">QuickEatz</Link> </div>
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
-                  <Link href="/trending">
-                    <a
-                      href="/trending"
-                      className="bg-gray-900 text-white px-3 py-1 rounded-md text-sm font-medium border-4 border-black hover:border-white"
-                    >
-                      Trending
+                    <Link href="/trending">
+                      <a
+                        href="/trending"
+                        className="bg-gray-900 text-white px-3 py-1 rounded-md text-sm font-medium border-4 border-black hover:border-white"
+                      >
+                        Trending
                     </a>
-                  </Link>
-                  <Link href="/dashboard">
-                    <a
-                      href="/dashboard"
-                      className="bg-gray-900 text-white px-3 py-1 rounded-md text-sm font-medium border-4 border-black hover:border-white"
-                    >
-                      Dashboard
+                    </Link>
+                    <Link href="/dashboard">
+                      <a
+                        href="/dashboard"
+                        className="bg-gray-900 text-white px-3 py-1 rounded-md text-sm font-medium border-4 border-black hover:border-white"
+                      >
+                        Dashboard
                     </a>
-                  </Link>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -69,9 +69,9 @@ export class NavBar extends React.Component {
                         aria-haspopup="true"
                       >
                         <span className="sr-only">Open user menu</span>
-                        <Image src="/images/profile.png" alt="Profile Image" width={32} height={32}/>
-                      </button> 
-                    </div> 
+                        <Image src="/images/profile.png" alt="Profile Image" width={32} height={32} />
+                      </button>
+                    </div>
                     {/*
                 Dropdown menu, show/hide based on menu state.
 
@@ -88,22 +88,22 @@ export class NavBar extends React.Component {
                       aria-orientation="vertical"
                       aria-labelledby="user-menu"
                     >
-                      <Link href="/profile">
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        role="menuitem"
-                      >
-                        Your Profile
+                      <Link href="/viewProfile">
+                        <a
+                          href="/viewProfile"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          Your Profile
                       </a>
                       </Link>
                       <Link href="/logout">
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        role="menuitem"
-                      >
-                        Sign out
+                        <a
+                          href="/logout"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          role="menuitem"
+                        >
+                          Sign out
                       </a>
                       </Link>
                     </div>
@@ -111,27 +111,27 @@ export class NavBar extends React.Component {
                 </div>
               </div>
               <div className="-mr-2 flex md:hidden">
-          {/*<!-- Mobile menu button -->*/}
-          <button type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span className="sr-only">Open main menu</span>
-            {/*--
+                {/*<!-- Mobile menu button -->*/}
+                <button type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                  <span className="sr-only">Open main menu</span>
+                  {/*--
               Heroicon name: outline/menu
 
               Menu open: "hidden", Menu closed: "block"
             */}
-            <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-            {/*
+                  <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                  </svg>
+                  {/*
               Heroicon name: outline/x
 
               Menu open: "block", Menu closed: "hidden"
             */}
-            <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
+                  <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
 
@@ -208,7 +208,7 @@ export class NavBar extends React.Component {
             </div>
           </div>
         </nav>
-      </div>
+      </div >
     );
   }
 }
