b697631f98598d5ea3a0c696fd1d000261d21700
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-08 22:24:39 -0400

    Revert "Revert "updated Vendor Dashboard""
    This reverts commit 11320697d45d4eb999e5c28e642ef791ad887212.

--- pages/viewVendor.js
+++ pages/viewVendor.js
@@ -11,4 +11,44 @@ export default function FirstPost() {
       <h1>View Vendor Page</h1>
     </Layout>
   )
+}import Head from 'next/head'
+
+export default function VendorPage() {
+	return (
+		<>
+			<Head>
+				<title>Vendor Page</title>
+			
+				<div class=topnav>
+  				<a class=active href=#home>Home</a>
+  				<a href=#about>About</a>
+  				<a href=#contact>Contact</a>
+  				<a href=#help>Help</a>
+  				<a href=#vendor>Vendor</a>
+  				<input type=text placeholder=Search...></input>
+  			
+		</div>
+				
+
+			</Head>
+
+			<h1>QUICKEATZ</h1>
+	<section>
+	<header>View A Vendor</header>
+	</section>
+	
+
+	<h2> What would you like to do today?</h2>
+
+
+	<button class=button button1>
+	<p1> Read Review</p1>
+	</button>
+	
+	<button class=button button2>
+	<p2> Create Review</p2>
+	</button>
+	</>
+
+	)
 }
