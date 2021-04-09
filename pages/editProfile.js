f7281608bd4b23fee6dba6f6bbf3020a2e1a6453
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-08 22:10:31 -0400

    Moved changes to this new branch

--- pages/editProfile.js
+++ pages/editProfile.js
@@ -9,4 +9,45 @@ export default function FirstPost() {
       <h1>Edit Profiles Page</h1>
     </div>
   )
+}import Head from 'next/head'
+
+
+export default function VenderProfile() {
+	return (
+		<>
+			<Head>
+				
+				<title>Edit Vendor Profile</title>
+					<div class=topnav>
+  				<a class=active href=#home>Home</a>
+  				<a href=#about>About</a>
+  				<a href=#contact>Contact</a>
+  				<a href=#help>Help</a>
+  				<a href=#vendor>Vendor</a>
+  				<input type=text placeholder=Search...></input>
+		</div>
+
+
+		<div class=myDiv></div>
+				
+			</Head>
+			<h1>QuickEatz</h1>
+			<section>
+			<header>Edit Vendor Profile</header>
+			</section>
+
+
+
+	<h2> What would you like to edit today?</h2>
+
+	<button class=button button1>
+	<p1> Settings</p1>
+	</button>
+	<button class=button button1>
+	<p2> Apply </p2>
+	</button>
+
+	</>
+
+	)
 }
