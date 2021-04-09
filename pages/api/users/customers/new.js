a39ff4ee247d50ba8172c61627eb2d768832e349
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-08 21:47:32 -0400

    Got POST request addUser to work

--- pages/api/users/customers/new.js
+++ pages/api/users/customers/new.js
@@ -1,48 +1,44 @@
 import { connectToDatabase } from "../../../../util/mongodb";
-import _ from 'lodash';
+import { formatNewCustomer } from "../../../../util/apiHelpers";
+import { body, validationResult } from "express-validator";
+import _ from "lodash";
 
-async function findCustomers(query) {
+async function findCustomer(email) {
   const { db } = await connectToDatabase();
-  console.log("query:", query);
-  const searchResult = await db
-    .collection("customers")
-    .find(query)
-    .toArray();
+  const query = {"email": email}
+  const searchResult = await db.collection("customers").find(query).toArray();
   console.log("SEARCH:", searchResult);
   return searchResult;
 }
 
-// export default async function(req, res) {
-//   return new Promise((resolve, reject) => {
-//     getData()
-//       .then(response => {
-//         res.statusCode = 200
-//         res.setHeader('Content-Type', 'application/json');
-//         res.setHeader('Cache-Control', 'max-age=180000');
-//         res.end(JSON.stringify(response))
-//         resolve();
-//       })
-//       .catch(error => {
-//         res.json(error);
-//         res.status(405).end();
-//         return resolve(); //in case something goes wrong in the catch block (as vijay) commented
-//       });
-//   });
-// };
+async function addUser(body) {
+  // Format Request Body
+  const reqBodies = formatNewCustomer(body.first_name, body.last_name, body.username, body.password, body.email);
+  const customersDoc = reqBodies[0];
+  const allUsersDoc = reqBodies[1];
+  console.log("customers:", customersDoc);
+  console.log("allUsers", allUsersDoc);
+  // Insert new document into 'customers' collection
+  const { db } = await connectToDatabase();
+  const result = await db.collection("customers").insertOne(customersDoc);
+  console.log("Result:", result);
+  return result;
+}
 
 export default async (req, res) => {
-  const { db } = await connectToDatabase();
-    if(req.method === "POST"){ // If creating a new account...
-      // First check if the user already exists:
-      console.log("REQUEST BODY:", req.body);
-      const customer = await findCustomers(req.body);
-      console.log("CUSTOMER:", customer);
-      if(!_.isEmpty(customer)){
-        res.send(customer);
-        res.end();
-      } else {
-        // If not, create a new user
-        res.status(400).json({"message":"You can create a new user."});
-      }
+  if (req.method === "POST") {
+    // If creating a new account...
+    // First check if the user already exists:
+    console.log("REQUEST BODY:", req.body);
+    const customer = await findCustomer(_.get(req,'body.email'));
+    console.log("CUSTOMER:", customer);
+    if (!_.isEmpty(customer)) {
+      // If user already exists, return error
+      res.status(400).json({ error: "User already exists." });
+    } else {
+      // If not, create a new user
+      const result = await addUser(req.body)
+      res.status(200).json({ message: "You can create a new user." });
     }
+  }
 };
