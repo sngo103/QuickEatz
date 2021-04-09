7095577bd78d4315c04109833020268c3e7b4f83
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-09 14:25:02 -0400

    modified title on customerdashboard page

--- components/CustomerDashboard.js
+++ components/CustomerDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from 'next/image'
+import styles from '../styles/CustomerDashboard.module.css'
 
 export default class CustomerDashboard extends React.Component {
   constructor(props) {
@@ -14,8 +15,8 @@ export default class CustomerDashboard extends React.Component {
       <div>
         <section className="h-40">
           <header className="bg-white shadow text-center">
-            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-              <h1 className="text-6xl font-bungee text-gray-900">Dashboard</h1>
+            <h1 className={styles.title}>Dashboard</h1>
+            <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
             </div>
           </header>
         </section>
@@ -48,7 +49,7 @@ export class CustomerDashboard extends React.Component {
       <div>
         <header className="bg-white shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Customer Dashboard</h1>
+            <h1 className="text-6xl font-bungee text-gray-900">Customer Dashboard</h1>
           </div>
         </header>
         <main>
