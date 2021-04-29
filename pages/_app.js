9f175d8440fc3da8be116a327c767135941136f0
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-29 15:21:12 -0400

    Fixed navBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,59 +1,60 @@
 import "../styles/globals.css";
 import ExtendedNavBar from "../components/ExtendedNavBar.js";
 import UserContext from "../components/UserContext";
-import App from 'next/app';
-import Router from 'next/router';
-import React from 'react';
+import App from "next/app";
+import Router from "next/router";
+import React from "react";
 
-export default class MyApp extends App{
-	state = {
-		email: "TEST"
-	};
-	componentDidMount = () => {
-		const email = localStorage.getItem('quickeatz_email');
-		if(email){
-			this.setState({email});
-		} else{
-			
-				Router.push('/'); //TOO ABRUPT MAYBE
-		}
-		
-		
-	};
-	
-	signIn = (email_in) => {
-		this.setState({email_in});
-	}
-	
-	signOut = () => {
-    this.setState({
-      email: null
-    });
-    Router.push('/');
+export default class MyApp extends App {
+  state = {
+    email: "TEST",
   };
-  render() {
-	  const { Component, pageProps } = this.props;
-  return (
-    <>
-      <style>
-        @import
-        url('https://fonts.googleapis.com/css2?family=Pridi:wght@300&display=swap');
-        @import
-        url('https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap');
-      </style>
+  componentDidMount = () => {
+    const email = localStorage.getItem("quickeatz_email");
+    if (email) {
+      this.setState({ email });
+      Router.push("/dashboard");
+    } else {
+      Router.push("/"); //TOO ABRUPT MAYBE
+    }
+  };
 
-	  <UserContext.Provider value={{email: this.state.user, signIn: this.signIn, signOut: this.signOut}}>
-      <ExtendedNavBar {...pageProps}/>
-	  </UserContext.Provider>
-      <main>
-	
-	  <UserContext.Provider value={{email: this.state.user, signIn: this.signIn, signOut: this.signOut}}>
-      <Component {...pageProps} />
-	  </UserContext.Provider>
-	  
-      </main>
-    </>
-  );
+  signIn = (email_in) => {
+    this.setState({ email_in });
+  };
+
+  signOut = () => {
+    this.setState({
+      email: null,
+    });
+    Router.push("/");
+  };
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Pridi:wght@300&display=swap');
+          @import
+          url('https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap');
+        </style>
+
+        <main>
+          <UserContext.Provider
+            value={{
+              email: this.state.user,
+              signIn: this.signIn,
+              signOut: this.signOut,
+            }}
+          >
+            <ExtendedNavBar {...pageProps} />
+            <Component {...pageProps} />
+          </UserContext.Provider>
+        </main>
+      </>
+    );
   }
 }
 
