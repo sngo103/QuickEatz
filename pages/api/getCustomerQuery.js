d384ad0e1bd7934283a29f1e61dee1464d58463f
TDLorenz <troydaniello.nyc@gmail.com>
2021-04-29 19:54:01 -0400

    tested db connection in getCustomerQuery

--- pages/api/getCustomerQuery.js
+++ pages/api/getCustomerQuery.js
@@ -1,2 +1,27 @@
 import { connectToDatabase } from "../../util/mongodb";
 const ObjectId = require('mongodb').ObjectID;
+
+
+export default async function handler(req, res) {
+    const { db } = await connectToDatabase();
+
+    /*
+    const data = req.query;
+
+    const user_id_str = data._id;
+    const user_id_param = new ObjectId(user_id_str);
+
+    const user_search_param = { "_id": user_id_param };
+
+    const response = await db.collection("vendors").findOne(user_search_param);
+    console.log(response);
+
+    res.json(response);
+
+    return response;
+    */
+
+    const data = await db.collection("vendors").find({}).limit(2).toArray();
+    console.log(data);
+    res.json(data);
+}
