c028b577fb697ae233a91ee6a12390466e416058
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-05-02 22:11:59 -0400

    Changed login page design

--- components/Login.js
+++ components/Login.js
@@ -11,13 +11,13 @@ class LoginForm extends React.Component {
       password: "",
       account_type: "customer",
       incorrect: false,
-      success: false,
+      success: false, 
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-	this.toggleCustomerLogin = this.toggleCustomerLogin.bind(this);
-	this.toggleVendorLogin = this.toggleVendorLogin.bind(this);
+    this.toggleCustomerLogin = this.toggleCustomerLogin.bind(this);
+    this.toggleVendorLogin = this.toggleVendorLogin.bind(this);
   }
 
   handleChange(event) {
@@ -40,11 +40,14 @@ class LoginForm extends React.Component {
       this.state.account_type
     );
     if (pass) {
-      const newToken = await refreshToken(this.state.email, this.state.account_type);
+      const newToken = await refreshToken(
+        this.state.email,
+        this.state.account_type
+      );
       localStorage.setItem("quickeatz_token", newToken);
       localStorage.setItem("quickeatz_email", this.state.email);
       localStorage.setItem("quickeatz", true);
-	  localStorage.setItem("quickeatz_type", this.state.account_type);
+      localStorage.setItem("quickeatz_type", this.state.account_type);
       //New stuff
       this.setState({ success: true });
       Router.push("/dashboard");
@@ -52,18 +55,142 @@ class LoginForm extends React.Component {
       this.setState({ incorrect: true });
     }
   }
-  
-  toggleCustomerLogin(){
-	  console.log("Switching to customer!");
-	  this.setState({account_type: "customer"});
+
+  toggleCustomerLogin() {
+    console.log("Switching to customer!");
+    this.setState({ account_type: "customer" });
   }
-  
-  toggleVendorLogin(){
-	  console.log("Switching to vendor!");
-	  this.setState({account_type: "vendor"});
+
+  toggleVendorLogin() {
+    console.log("Switching to vendor!");
+    this.setState({ account_type: "vendor" });
   }
 
   render() {
+    const customerLogin = (
+      <div class="flex items-center justify-center">
+        <form class="w-1/2 space-y-6" onSubmit={this.handleSubmit}>
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="email-address" class="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+                class={this.state.account_type === "customer" ? "appearance-none rounded-none relative block w-full px-3 py-2 placeholder-gray-500 text-gray-900 focus:outline-none ring-4 ring-yellow-300 z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none"}
+                placeholder="Email address"
+                onChange={this.handleChange}
+                value={this.state.email}
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class={this.state.account_type === "customer" ? "appearance-none rounded-none relative block w-full px-3 py-2 placeholder-gray-500 text-gray-900 focus:outline-none ring-4 ring-yellow-300 z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none"}
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={this.state.password}
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-300 hover:bg-yellow-400"
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                class="h-5 w-5 text-yellow-400 group-hover:text-yellow-300"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign in
+          </button>
+        </form>
+      </div>
+    );
+
+    const vendorLogin = (
+      <div class="flex items-center justify-center">
+        <form class="w-1/2 space-y-6" onSubmit={this.handleSubmit}>
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="email-address" class="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+                class={this.state.account_type === "vendor" ? "appearance-none rounded-none relative block w-full px-3 py-2 placeholder-gray-500 text-gray-900 focus:outline-none ring-4 ring-yellow-600 z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none"}
+                placeholder="Email address"
+                onChange={this.handleChange}
+                value={this.state.email}
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class={this.state.account_type === "vendor" ? "appearance-none rounded-none relative block w-full px-3 py-2 placeholder-gray-500 text-gray-900 focus:outline-none ring-4 ring-yellow-600 z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none"}
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={this.state.password}
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-500"
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                class="h-5 w-5 text-yellow-400 group-hover:text-yellow-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign in
+          </button>
+        </form>
+      </div>
+    );
+
     return (
       <div className="container inline-block p-6 text-center font-pridi text-lg">
         <Image
@@ -82,83 +209,22 @@ class LoginForm extends React.Component {
         ) : null}
 
         <div class="flex items-center justify-center">
-          <div class="w-1/2 space-y-6">
+          <div className="w-full">
             <button
               onClick={this.toggleCustomerLogin}
-              class="w-1/3 py-2 mx-2 text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-600"
+              class={this.state.account_type === "customer" ? "w-1/4 py-2 px-6 text-sm font-medium text-white rounded-t-md bg-yellow-300 focus:outline-none ring-2 ring-offset-2 ring-yellow-300" : "w-1/4 py-2 px-6 text-sm font-medium text-white rounded-t-md bg-yellow-300 hover:bg-yellow-200 focus:outline-none"}
             >
               Customer
             </button>
             <button
               onClick={this.toggleVendorLogin}
-              class="w-1/3 justify-center py-2  text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              class={this.state.account_type === "vendor" ? "w-1/4 py-2 px-6 text-sm font-medium text-white rounded-t-md bg-yellow-600 focus:outline-none ring-2 ring-offset-2 ring-yellow-600" : "w-1/4 py-2 px-6 text-sm font-medium text-white rounded-t-md bg-yellow-600 hover:bg-yellow-500 focus:outline-none"}
             >
               Vendor
             </button>
           </div>
-          <br />
-          <br />
-          <br />
         </div>
-        <div class="flex items-center justify-center">
-          <form class="w-1/2 space-y-6" onSubmit={this.handleSubmit}>
-            <div class="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label for="email-address" class="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  required
-                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                  onChange={this.handleChange}
-                  value={this.state.email}
-                />
-              </div>
-              <div>
-                <label for="password" class="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                  onChange={this.handleChange}
-                  value={this.state.password}
-                />
-              </div>
-            </div>
-            <button
-              type="submit"
-              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-400"
-            >
-              <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  class="h-5 w-5 text-yellow-400 group-hover:text-yellow-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </span>
-              Sign in
-            </button>
-          </form>
-        </div>
+        {this.state.account_type === "vendor" ? vendorLogin : customerLogin}
       </div>
     );
   }
