04aa4383a68601c8d313df22de3232db7c69571a
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-05-17 21:07:18 -0400

    Cleaned up vendor single display pages

--- pages/viewVendorSingle.js
+++ pages/viewVendorSingle.js
@@ -1,14 +1,9 @@
-import Head from "next/head";
 import React from "react";
 import VendorDisplaySingle from "../components/VendorDisplaySingle";
 
 export default function VendorDisplayPage() {
   return (
     <div>
-      <Head>
-        <title>Vendor Page</title>
-      </Head>
-
       <VendorDisplaySingle />
     </div>
   );
